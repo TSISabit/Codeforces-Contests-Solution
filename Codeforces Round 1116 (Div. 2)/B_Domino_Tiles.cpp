6c90b9673f49459497a3eca091d48381d373c616
TSISabit <tariksaidulislam@gmail.com>
2026-08-10 17:30:35 +0600

    Added Problem B in the Codeforces Round 1116 (Div. 2)

--- Codeforces Round 1116 (Div. 2)/B_Domino_Tiles.cpp
+++ Codeforces Round 1116 (Div. 2)/B_Domino_Tiles.cpp
@@ -13,8 +13,26 @@ using namespace std;
 #define lcm(a, b) ((a) / gcd(a, b) * (b))
 #define vi vector<int>
 
-void solve() {
 
+const int MOD = 998244353; 
+bool check(string &s, int start, int first){
+	for(int i = start; i < sz(s); i += 2){
+		int step = (i - start) / 2; 
+		int ex = first ^ (step % 2); 
+		if(s[i] != '?' && (s[i] - '0') != ex) return false; 
+	}
+	return true; 
+}
+void solve() {
+	read(n); 
+	string s; 
+	cin >> s; 
+	int odd = check(s, 0, 0) + check(s, 0, 1); 
+	int even = check(s, 1, 0) + check(s, 1, 1); 
+
+	int ans = (odd * even); 
+
+	cout << ans << nn; 
 }
 
 int32_t main() {
